946f5f47a15c970a750c22adf7a81e8021a00d6e
unknown <lukacc008@gmail.com>
2024-02-19 19:09:50 +0100

    Added Header and UserInput component with styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import Header from "./components/Header";
+import UserInput from "./components/UserInput";
+
 function App() {
   return (
-    <h1>React Investment Calculator</h1>
-  )
+    <div>
+      <Header />
+      <UserInput />
+    </div>
+  );
 }
 
-export default App
+export default App;
