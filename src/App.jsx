611dd6a6caa31fe7c2ad7df480b4c3fd9667bd73
unknown <lukacc008@gmail.com>
2024-02-22 00:23:00 +0100

    Conditionally rendering the table if user enters invalid value, also a feedback message is shown

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,8 @@ function App() {
     duration: 10,
   });
 
+  const inputIsValid = userInput.duration >= 1;
+
   function handleChange(inputIdentifier, newValue) {
     setUserInput((prevUserInput) => {
       return {
@@ -25,7 +27,8 @@ function App() {
     <div>
       <Header />
       <UserInput onChangeInput={handleChange} userInput={userInput} />
-      <Results userInput={userInput} />
+      {!inputIsValid && <p className="center">Please enter a duration greater than zero.</p>}
+      {inputIsValid && <Results userInput={userInput} />}
     </div>
   );
 }
